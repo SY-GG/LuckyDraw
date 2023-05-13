3602a56093cf2e1d20b5deac45d167210bc7f840
error <sy.park.HUB@gmail.com>
2023-05-13 10:37:41 +0900

    웹훅 연결 index 파일

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-export const handler = async(event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const axios = require('axios');
+
+export const handler = async (event) => {
+  // TODO implement
+  try {
+    const result = await axios.post(
+      '웹훅url',
+      {
+        content: '봇 웹훅 테스트 결과',
+      }
+    );
+    console.info('연결 성공');
+  } catch (err) {
+    console.err('연결 실패', err);
+  }
 };
